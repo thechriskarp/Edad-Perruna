b68c3221afc4d7bfcf565b28ab9af8fb1cec5e1d
Chris C. Karp <www.chriskarp@gmail.com>
2026-03-04 23:51:32 -0300

    Add files via upload

---
+++ conversionEdad.cxx
@@ -0,0 +1,24 @@
+//Dicen que 1 año de humano equivale a 7 años de perro. Vamos a hacer un programa que convierta tu edad a años perrunos.
+//​Lo que necesitas:
+//​La constante: El número 7.
+//​La entrada: Tu edad (un número entero).
+//​El cálculo: Tu edad multiplicado por 7.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	const int MULTIPLICADOR = 7;
+	int edad;
+	
+	cout << "Ingresa tu edad: ";
+	cin >> edad;
+	
+	edad = 7 * MULTIPLICADOR;
+	
+	cout << "Tu edad es: " << edad << endl;
+	
+	return 0;
+	
+}
+	
